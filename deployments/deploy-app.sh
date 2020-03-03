df149d705e7a05b8f2fc2524e4d1c309d038f1ea
Kiran <kiran@my.k8s.com>
2020-03-03 18:26:48 +0530

    dfrefre

--- deployments/deploy-app.sh
+++ deployments/deploy-app.sh
@@ -1,5 +1,5 @@
 cd ~/3-tier-k8s/deployments
-sed -i "s,${FRONTEND_IMAGE_NAME},557663480133.dkr.ecr.eu-west-2.amazonaws.com/cloudhands/demo-frontend:latest,g" deploy-app.yaml
-sed -i "s,${SERVER_IMAGE_NAME},557663480133.dkr.ecr.eu-west-2.amazonaws.com/cloudhands/demo-app:latest,g" deploy-server.yaml
+sed -i "s,\${FRONTEND_IMAGE_NAME},557663480133.dkr.ecr.eu-west-2.amazonaws.com/cloudhands/demo-frontend:latest,g" deploy-app.yaml
+sed -i "s,\${SERVER_IMAGE_NAME},557663480133.dkr.ecr.eu-west-2.amazonaws.com/cloudhands/demo-app:latest,g" deploy-server.yaml
 
 kubectl apply -k .
