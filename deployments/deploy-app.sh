c6471454a69e43e226c5d85a0da9a17d5c89cd57
Kiran <kiran@my.k8s.com>
2020-03-03 16:03:24 +0530

    dfrefre

--- deployments/deploy-app.sh
+++ deployments/deploy-app.sh
@@ -1,5 +1,5 @@
 cd ~/3-tier-k8s/deployments
-sed -f k8s-dev.cloudhands.online.sed deploy-app.yaml
-sed -f k8s-dev.cloudhands.online.sed deploy-server.yaml
+sed -i k8s-dev.cloudhands.online.sed deploy-app.yaml
+sed -i k8s-dev.cloudhands.online.sed deploy-server.yaml
 
 kubectl apply -k .
